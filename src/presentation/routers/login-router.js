5d52dc32316f8862670fc94a1cd5c774a866b59f
wesleycbl <wesleypsantos@hotmail.com>
2022-03-24 15:45:24 -0300

    ensure LoginRouter return 200 when valid credentials are provided

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -17,7 +17,11 @@ module.exports = class LoginRouter {
       return HttpResponse.badRequest('password')
     }
 
-    this.authUseCase.auth(email, password)
-    return HttpResponse.unauthorizedError('password')
+    const accessToken = this.authUseCase.auth(email, password)
+
+    if (!accessToken) {
+      return HttpResponse.unauthorizedError()
+    }
+    return HttpResponse.ok()
   }
 }
